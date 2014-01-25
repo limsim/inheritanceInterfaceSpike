35d2b7cb57133febd63f372397125707561d926c
lim sim <Lim.Sim@Gamesys.co.uk>
2014-01-25 20:09:21 +0000

    introduced Spring DI

--- src/main/java/org/theSpike/TheUser.java
+++ src/main/java/org/theSpike/TheUser.java
@@ -1,19 +1,35 @@
 package org.theSpike;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class TheUser {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-config.xml"})
+public class TheUser implements ApplicationContextAware{
+
+    ApplicationContext applicationContext = null;
+
     @Test
     public void myTest() {
-        theInterface something = new OtherClass();
+        theInterface something = (theInterface) applicationContext.getBean("theClass");
         something.method1();
         something.method2();
     }
 
     @Test
     public void dummy() {
-        theInterface something = new theClass();
+        theInterface something = (theInterface) applicationContext.getBean("OtherClass");
         something.method1();
         something.method2();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
